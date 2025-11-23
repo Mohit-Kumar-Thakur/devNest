e44eb1c0e350a03525b9bba26e9f1b3fc02d4c41
Nizamuddin1N <159190139+Nizamuddin1N@users.noreply.github.com>
2025-11-23 23:48:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return conn;
     } catch (error) {
         console.error("MongoDB Connection Failed:", error.message);
         process.exit(1);
